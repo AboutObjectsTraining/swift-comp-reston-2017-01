5cbe1b53cc3c6743d734aa0954036f9f74375150
Jonathan Lehr <jonathan@aboutobjects.com>
2017-01-19 14:49:58 -0500

    Refactor storyboard

--- lab exercises/ReadingList/ReadingList/ReadingListController.swift
+++ lab exercises/ReadingList/ReadingList/ReadingListController.swift
@@ -4,4 +4,7 @@ class ReadingListController: UITableViewController
 {
     @IBAction func doneEditing(segue: UIStoryboardSegue) { }
     @IBAction func cancelEditing(segue: UIStoryboardSegue) { }
+    
+    @IBAction func doneAdding(segue: UIStoryboardSegue) { }
+    @IBAction func cancelAdding(segue: UIStoryboardSegue) { }
 }
